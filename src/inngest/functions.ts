8b4908b676fab9c593fb413cadacd05e76f29ef3
encokarthur298 <endev1569@gmail.com>
2025-07-11 16:17:55 +0100

    added ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from "./client";
+import { Agent, gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event}) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert nextjs developer.  You write readable, concise code.",
+      model: gemini({ model: "gemini-2.0-flash",apiKey:process.env.GEMINI_API_KEY }),
+    });
+    const { output } = await codeAgent.run(
+      'Write this code: ' + event.data.value,
+    );
+    console.log(output);
+    // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
+   
+    return {output};
   },
 );
